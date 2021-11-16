5346e5d2c79d1a770c58735c25d34f66c196a86a
AmberMaks <73847004+AmberMaks@users.noreply.github.com>
2021-11-16 16:50:58 +0300

    практика 3 урок 1 номер

--- new-feature.go
+++ new-feature.go
@@ -1,1 +1,30 @@
-some content
+using System;
+
+namespace practical3__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var massiv = new int[3, 3];
+            var elem = new int[3];
+            int k = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    massiv[i, j] = int.Parse(Console.ReadLine());
+                    if (i == j)
+                    {
+                        elem[k] = massiv[i, j];
+                        k += 1;
+                    }
+                }
+            }
+            for (int z = 0; z < 2; z++)
+            {
+                Console.WriteLine($"{elem[z]}");
+            }
+        }
+    }
+}
